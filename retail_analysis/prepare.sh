afc834e59386fdcfc90085d9e95d85901e9d6778
hugolu <printk@gmail.com>
2015-12-11 07:24:38 +0000

    download, unzip, and then import to mysql db

---
+++ retail_analysis/prepare.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# download & unzip data
+wget http://tinyurl.com/hdfiles-zip
+rm -r hdfiles
+mkdir hdfiles
+unzip hdfiles-zip -d hdfiles
+
+# import data to mysql
+mysql -uhive -phive -e "DROP DATABASE IF EXISTS test"
+mysql -uhive -phive -e "CREATE DATABASE test"
+mysql -uhive -phive test < hdfiles/retail_db.sql
+mysql -uhive -phive -e "SHOW TABLES" test
